39334e0840c7f2763f2d2bb54b6d96b7cf1db260
vmunson <virginia.munson@gmail.com>
2018-05-22 16:27:47 -0400

    typo fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
           <p>Munson Haus is being built.<br/> Check out our divisions -
             <a href="https://vmunson.github.io/portfolio/"> Developing </a>
             ,<a href="http://virginiamunson.wixsite.com/videoportfolio">Productions </a>
-            , and <a href="https://emilymunson.com/"> Writings</a>.
+            ,and <a href="https://emilymunson.com/"> Writings</a>.
           </p>
           <img src={logo}/>
       </div>
