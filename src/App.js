974b78a477cbb0eab048ddd0b7cf06c8d2ea3a88
vmunson <virginia.munson@gmail.com>
2018-05-22 15:49:55 -0400

    being built update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import logo from './assets/munsonHausLogo copy.png'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-          Munson Haus 
+          <p>Munson Haus is being built.<br/> Check out our divisions -
+            <a href="https://vmunson.github.io/portfolio/"> Developing </a>
+            ,<a href="http://virginiamunson.wixsite.com/videoportfolio">Productions </a>
+            , and <a href="https://emilymunson.com/"> Writings</a>.
+          </p>
+          <img src={logo}/>
       </div>
     );
   }
