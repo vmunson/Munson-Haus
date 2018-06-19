70a5fac501dff318605f1e7e3e404c276535021e
vmunson <virginia.munson@gmail.com>
2018-06-19 15:02:08 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div>
           <p>Munson Haus is being built.<br/> Check out our divisions -
-            <a href="https://vmunson.github.io/portfolio/" target="_blank"> Developing</a>
-            ,<a href="http://virginiamunson.wixsite.com/videoportfolio" target="_blank"> Productions</a>
-            , and <a href="https://emilymunson.com/" target="_blank"> Writings</a>.
+            <a href="https://vmunson.github.io/portfolio/" target="_blank"> Develop</a>
+            ,<a href="http://virginiamunson.wixsite.com/videoportfolio" target="_blank"> Production</a>
+            , and <a href="https://emilymunson.com/" target="_blank"> Writing</a>.
           </p>
           <img src={logo} alt="a house with munson haus coming out of its chimney"/>
       </div>
